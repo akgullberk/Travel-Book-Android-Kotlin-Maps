8fc0115463a206a948cacea9f682589c0bf37ed0
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-12 17:02:20 +0300

    main and maps activity uı

--- app/src/main/java/com/example/kotlinmaps/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmaps/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kotlinmaps
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_place){
+            val intent = Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
